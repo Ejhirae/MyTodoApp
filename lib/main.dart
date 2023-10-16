4bcc8883338cebd5c894a076e2e4aa8a8adb0892
ejhirae <victor.osahon24@gmail.com>
2023-10-16 18:17:38 +0100

    Intermediate Features Included, check ReadMe

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:  MyTodoApp(title: 'My Todo App',),
+      home:  MyTodoApp(title: 'MY TASKS',),
     );
   }
 }
